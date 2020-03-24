1b9431e615234031a43237f0b382538717227694
briannabeshkin <45013199+briannabeshkin@users.noreply.github.com>
2020-03-24 18:45:37 -0400

    a

--- asst_2/main.js
+++ asst_2/main.js
@@ -10,4 +10,20 @@ const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 
+for (x=1; x < 6; x++) {
+    loop(x);
+}
+
+function loop(src) {
+    thumBar = document.querySelector('.thumb-bar').write('pic' + src);
+    newImage.onclick() 
+}
 /* Wiring up the Darken/Lighten button */
+function className() {
+    const cl = btn.getAttribute('class');
+    if (cl === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.innerHTML('lighten');
+        document.body.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+}
