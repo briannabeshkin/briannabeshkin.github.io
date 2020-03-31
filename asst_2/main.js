acc550b626fa59487a7c7ab0fcae660c5fed9aae
briannabeshkin <45013199+briannabeshkin@users.noreply.github.com>
2020-03-31 11:44:12 -0400

    update

--- asst_2/main.js
+++ asst_2/main.js
@@ -8,24 +8,35 @@ const overlay = document.querySelector('.overlay');
 let imgs = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Looping through images */
-for (i = 0; i < imgs.length; i++) {
-document.querySelector('.thumb-bar').innerHTML += "<img src='" + imgs[i] + "' class='images'></img>";
+const newImage = document.createElement('img');
+newImage.setAttribute('src', 'xxx');
+thumbBar.appendChild(newImage);
+
+for (let i = 0; i < imgs.length; i++) {
+    newImage.setAttribute('src',imgs[i])
+    //document.querySelector('.thumb-bar').innerHTML += "<img src='" + imgs[i] + "' class='images'></img>";
+    newImage.onclick = function() {
+        displayedImage.setAttribute('src',imgs[i])
+    }
 }
 
 /* Wiring up the Darken/Lighten button */
 
-const btntxt = btn.getAttribute('class');
-if (btntxt === 'dark') {
-    btn.onclick = function(){
-        btn.setAttribute('class', 'light');
-        btn.textContent ='Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    };
-} 
-if (btntxt === 'light') {
-    btn.onclick = function(){
-        btn.setAttribute('class', 'dark');
-        btn.textContent ='Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    };
+let count = 0;
+document.querySelector(".dark").addEventListener("click", myFunction);
+
+function myFunction() {
+
+if(count % 2 == 0){
+    btn.setAttribute('class', 'light');
+    btn.textContent ='Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+count++;
+}else{
+    btn.setAttribute('class', 'dark');
+    btn.textContent ='Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    count++;
+
+}
 }
